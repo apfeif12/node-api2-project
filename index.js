2adfe4722df146bf011ab1f9c4db9b488b93a029
alex-pfeifer <apfeif.12@gmail.com>
2021-04-08 14:35:59 -0700

    deploy3

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ const port = process.env.PORT;
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const path = require("path");
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use("/api/", (_, res) => {
+    res.json({ data: "API working" });
+});
+
